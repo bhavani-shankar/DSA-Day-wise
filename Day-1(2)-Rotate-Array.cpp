a914ac4405309d07f1c06767548e464d9e2c74e7
Bhavani Shankar Purohit <bhavani.shankar037@gmail.com>
2021-10-03 14:18:14 +0530

    Add files via upload

---
+++ Day-1(2)-Rotate-Array.cpp
@@ -0,0 +1,7 @@
+void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end());
+    }
